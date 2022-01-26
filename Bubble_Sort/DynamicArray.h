25b787b5bec6e9b79146b7b26119defabab6e76c
Will <montero_w@live.com>
2022-01-26 15:14:40 -0600

    Created comments

--- Bubble_Sort/DynamicArray.h
+++ Bubble_Sort/DynamicArray.h
@@ -7,64 +7,113 @@ public:
 	DynamicArray();
 	~DynamicArray();
 
-	void addItem(T* item);
-	bool removeItem(T* item);
+	void addItems(T item[], int size);
+	void addItem(T item);
+	bool removeItem(T item);
 	void sortItems();
-	T* getItem(int index);
+	T getItem(int index);
+	int getLength() { return m_length; };
 private:
-	T** m_items;
+	T* m_items;
 	int m_length;
 
 };
 
+/// <summary>
+/// Creats the base structor of this class 
+/// </summary>
+/// <typeparam name="T"></typeparam>
 template<typename T>
 inline DynamicArray<T>::DynamicArray()
 {
-
+	m_length = 0;
+	m_items = nullptr;
 }
 
+/// <summary>
+/// Dealocates lose information once this class is removed 
+/// </summary>
+/// <typeparam name="T"></typeparam>
 template<typename T>
 inline DynamicArray<T>::~DynamicArray()
 {
-	delete[] m_items;
+
 }
 
 template<typename T>
-inline void DynamicArray<T>::addItem(T* item)
+inline void DynamicArray<T>::addItems(T items[], int size)
 {
-	T** temp = new T[m_length + 1];
-	for (int i = 0; i < m_length; i++)
+	for (int i = 0; i < size; i++)
+		addItem(items[i]);
+	
+}
+
+/// <summary>
+/// Handles the addition that is going to happen in the algorithum
+/// </summary>
+/// <typeparam name="T"> templete type</typeparam>
+/// <param name="item">item being added to </param>
+template<typename T>
+inline void DynamicArray<T>::addItem(T item)
+{
+	//Temparay array that will hold our values
+	T* temp = new T [getLength() + 1];
+	
+	//idarates through the list of items 
+	for (int i = 0; i < getLength(); i++)
+
+		//Sets temps current index to be items current index 
 		temp[i] = m_items[i];
 
-	temp[m_length] = item;
+	//sets the index equal to be the new item
+	temp[getLength()] = item;
+	//Incramnet by one 
 	m_length++;
 
+	//deletes the pointer 
 	delete[] m_items;
+	//sets our array of items to be temp
 	m_items = temp;
 	
 
 }
 
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <param name="item"></param>
+/// <returns></returns>
 template<typename T>
-bool DynamicArray<T>::removeItem(T* item)
+bool DynamicArray<T>::removeItem(T item)
 {
+	//Cheaks to 
+	if (!item || getLength() <= 0)
+		return false;
+
 	int j = 0;
 	bool removed = false;
-	T** temp = new T[m_length - 1];
-	for (int i = 0; i < m_length; i++)
+
+	T* temp = new T [getLength() - 1];
+
+	for (int i = 0; i < getLength(); i++)
 	{
-		if (m_items[i] != item)
+		if (m_items[i] != item || removed)
 		{
 			temp[j] = m_items[i];
 			j++;
 		}
 		else
+		{
 			removed = true;
+		}
 	}
+
 	if (removed)
 	{
 		delete[] m_items;
 		m_items = temp;
+		m_length--;
 	}
 	
 	return removed;
@@ -74,15 +123,15 @@ template<typename T>
 void DynamicArray<T>::sortItems()
 {
 	int j = 0;
-	int key = 0
+	T key;
 
-	for(int i = 1; i < m_length; i++)
+	for(int i = 1; i < getLength(); i++)
 	{
 		key = m_items[i];
 		j = i - 1;
-		while (i < 0 && m_items[j] > kay)
+		while (j >= 0 && m_items[j] > key)
 		{
-			if (m_items[j] > kay)
+			if (m_items[j] > key)
 			{
 				m_items[j + 1] = m_items[j];
 				j--;
@@ -94,7 +143,7 @@ void DynamicArray<T>::sortItems()
 }
 
 template<typename T>
-T* DynamicArray<T>::getItem(int index)
+T DynamicArray<T>::getItem(int index)
 {
 	return m_items[index];
 }
--- Bubble_Sort/main.cpp
+++ Bubble_Sort/main.cpp
@@ -1,7 +1,28 @@
 #include "Engine.h"
+#include "DynamicArray.h"
+#include <iostream>
+
+void printDynamicArray(DynamicArray<int> arr) {
+	for (int i = 0; i < arr.getLength(); i++)
+		std::cout << arr.getItem(i) << std::endl;
+	system("pause");
+}
 
 void main() {
-	Engine<int>* program = new Engine<int>();
-	program->run();
+	DynamicArray<int> arr = DynamicArray<int>();
+
+	arr.addItem(4);
+	arr.addItem(3);
+	arr.addItem(5);
+	arr.addItem(5);
+	arr.addItem(2);
+
+	printDynamicArray(arr);
+	
+	arr.sortItems();
+	arr.removeItem(5);
+	arr.removeItem(5);
+
+	printDynamicArray(arr);
 }
 
